8ea9f9d4a421173e5c4ebcc208366b126ce7fc3a
Thibault Lescoat <thibault.lescoat@telecom-paristech.org>
2017-11-20 17:51:43 +0100

    Update bx

--- cmake/bx.cmake
+++ cmake/bx.cmake
@@ -37,6 +37,9 @@ endif()
 # Add include directory of bx
 target_include_directories( bx PUBLIC ${BX_DIR}/include )
 
+# Add header-only externals
+target_include_directories( bx PRIVATE ${BX_DIR}/3rdparty )
+
 # Build system specific configurations
 if( MSVC )
 	target_include_directories( bx PUBLIC ${BX_DIR}/include/compat/msvc )
